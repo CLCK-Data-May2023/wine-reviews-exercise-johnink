116da86c34a4f66b6fab4d6df047fcc679fb8db9
johnink <johneatspie@gmail.com>
2023-08-31 10:37:20 -0400

    Update reviews.py

--- reviews.py
+++ reviews.py
@@ -1,2 +1,10 @@
 # add your code here
+import pandas as pd
+
+reviews = pd.read_csv('data\winemag-data-130k-v2.csv.zip', index_col=0)
+
+reviews_country = reviews.groupby(['country']).points.agg([count,mean])
+
+with open('data\\reviews-per-country.csv', 'w') as csv_file:
+    reviews_country.to_csv(path_or_buf=csv_file)
 
